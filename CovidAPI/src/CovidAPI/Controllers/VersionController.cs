658578e13993f39235d84ca6f4d74b21f3c7746a
scott.baldwin <scott.baldwin@purple.telstra.com>
2021-05-23 13:19:06 +1000

    Add Version Controller for test purposes

---
+++ CovidAPI/src/CovidAPI/Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CovidAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "1.0.0";
+        }
+    }
+}
