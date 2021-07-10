242f621f1b359b673156bcdbea25e735e2aed161
scott.baldwin <scott.baldwin@purple.telstra.com>
2021-07-10 14:41:04 +1000

    Add Lifecycle Hooks

--- CovidAPI/src/LifecycleHooks/Function.cs
+++ CovidAPI/src/LifecycleHooks/Function.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.CodeDeploy;
+using Amazon.CodeDeploy.Model;
+using LifecycleHooks.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -12,16 +15,59 @@ namespace LifecycleHooks
 {
     public class Function
     {
+        private static List<string> _environmentVariables = new List<string> {
+            "APPLICATION_NAME",
+            "DEPLOYMENT_ID",
+            "DEPLOYMENT_GROUP_NAME",
+            "DEPLOYMENT_GROUP_ID",
+            "LIFECYCLE_EVENT",
+            "env"
+        };
+
+        private static AmazonCodeDeployClient _codeDeployClient = null;
+        private static AmazonCodeDeployClient CodeDeployClient
+        {
+            get 
+            {
+                if (_codeDeployClient == null)
+                {
+                    _codeDeployClient = new AmazonCodeDeployClient();
+                }
+                return _codeDeployClient;
+            }
+        }
         
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
-        /// <param name="input"></param>
+        /// <param name="lifecycleEvent"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async Task<string> FunctionHandler(LifeCycleEventHook lifecycleEvent, ILambdaContext context)
         {
-            return input?.ToUpper();
+            Console.WriteLine($"DeploymentId: {lifecycleEvent.DeploymentId}");
+            Console.WriteLine($"LifecycleEventHookExecutionId: {lifecycleEvent.LifecycleEventHookExecutionId}");
+
+            // Let's take a look at all of the environment variables we get access to
+            Console.WriteLine("----- Environment Variables ------");
+            foreach (var name in _environmentVariables)
+            {
+                WriteEnvironmentVariable(name);
+            }
+
+            await CodeDeployClient.PutLifecycleEventHookExecutionStatusAsync(
+                new PutLifecycleEventHookExecutionStatusRequest{
+                    DeploymentId = lifecycleEvent.DeploymentId,
+                    LifecycleEventHookExecutionId = lifecycleEvent.LifecycleEventHookExecutionId,
+                    Status = LifecycleEventStatus.Succeeded
+                });
+            return "";
+        }
+
+        public void WriteEnvironmentVariable(string variableName)
+        {
+            var envVar = System.Environment.GetEnvironmentVariable(variableName);
+            Console.WriteLine($"variableName: {envVar}");
         }
     }
 }
