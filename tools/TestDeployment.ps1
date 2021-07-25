6f728aad88ac3703964a3bca4a6deb4db67c0f3c
scott.baldwin <scott.baldwin@purple.telstra.com>
2021-07-25 21:52:12 +1000

    Add script to demonstrate deployment rollback

---
+++ tools/TestDeployment.ps1
@@ -0,0 +1,60 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $Environment = "Prod"
+)
+
+$awsProfile = "sbaldwin-$($Environment.ToLower())"
+$stackName = "CovidSafe-$Environment-Stack"
+$apiUrl = ((aws cloudformation describe-stacks --stack-name $stackName --profile $awsProfile | ConvertFrom-Json).Stacks[0].Outputs | Where-Object OutputKey -eq "ApiURL").OutputValue
+
+$versionUrl = "$apiUrl/api/Version"
+$valuesurl = "$apiUrl/api/Values"
+
+Write-Verbose "Stack Name: $stackName, AWS Profile: $awsProfile, API URL: $apiUrl"
+
+$errorCount = 0
+$currentVersion = ""
+$counter = 0
+$bgColor = [System.ConsoleColor]::Blue
+if ($Environment -eq "Prod") {
+    $bgColor = [System.ConsoleColor]::Red
+}
+$fgColors = @([System.ConsoleColor]::Green, [System.ConsoleColor]::Magenta)
+$versionCount = 0
+$col = @{}
+$fg = [System.ConsoleColor]::White
+for ($i = 0; $i -lt 1000000; $i++) {
+    $versionResult = Invoke-WebRequest -Uri $versionUrl
+    $valuesResult = Invoke-WebRequest -Uri $valuesurl -SkipHttpErrorCheck
+
+    if ($versionResult.StatusCode -eq 200){
+        if ($currentVersion -ne $versionResult.Content) {
+            $counter = 0
+            $currentVersion = $versionResult.Content
+            if (-not $col.ContainsKey($currentVersion)) {
+                $col[$currentVersion] = $fgColors[$versionCount++]
+            }
+            $fg = $col[$currentVersion]
+            Write-Host ""
+            Write-Host "$Environment -" -NoNewline -BackgroundColor $bgColor
+            Write-Host " Version: $currentVersion" -ForegroundColor $fg -NoNewline
+        } else {
+            $counter += 1
+            Write-Host "." -NoNewline -ForegroundColor $fg
+            if ($counter % 10 -eq 0) {
+                $currentVersion = ""
+            }
+        }
+    }
+
+    if ($valuesResult.StatusCode -ne 200) {
+        $errorCount += 1
+        Write-Host "E" -ForegroundColor Red -NoNewline
+    }
+}
+
+Write-Host "#"
+
+Write-Host "Version hash table contains $($versionHash.Count) versions"
