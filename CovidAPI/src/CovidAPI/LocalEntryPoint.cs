0c120890e68737d77da2d398b0f4406a5e2446a3
scott.baldwin <scott.baldwin@purple.telstra.com>
2022-04-26 12:58:17 +1000

    Break build to test notification

--- CovidAPI/src/CovidAPI/LocalEntryPoint.cs
+++ CovidAPI/src/CovidAPI/LocalEntryPoint.cs
@@ -20,7 +20,7 @@ namespace CovidAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startu>();
                 });
     }
 }
