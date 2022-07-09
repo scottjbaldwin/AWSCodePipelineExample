edeeb6f7fd6f1e64584691e738cbbdbfa0797711
scott.baldwin <scott.baldwin@purple.telstra.com>
2022-07-09 17:27:51 +1000

    Add pwsh script to deploy isolated stack
    - Calculate git commit
    - Use dotnet lambda deploy-serverless to deploy template

---
+++ tools/Deploy-IsolatedStack.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $IsolatedStackName,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $CFNBucket,
+
+    [Parameter()]
+    [string]
+    $Environment = "Dev"
+)
+
+
+$gitCommit = git rev-parse --short HEAD
+
+$stat = git status -s
+if ($stat.Count -gt 0) {
+    Write-Verbose "Current branch not clean, there are changes since the last commit"
+    $gitCommit += "X"
+}
+$samTemplate = Join-Path $PSScriptRoot "/../CovidAPI/src/CovidApi/covidapi.yml"
+
+Write-Verbose "Deploying $samTemplate to isolated stack git commit $gitCommit"
+
+dotnet lambda deploy-serverless "$IsolatedStackName-CovidSafe" `
+    -t $samTemplate `
+    -sb $CFNBucket `
+    -sp "$IsolatedStackName/" `
+    -tp "Environment=$Environment;IsolatedStackPrefix=$IsolatedStackName-" `
+    --msbuild-parameters "/p:VersionSuffix=isolated-stack-$gitCommt"
